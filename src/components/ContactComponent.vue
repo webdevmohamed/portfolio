0d4d3cefb86d95d9b69b796f694b13f10eb6d5fc
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2024-04-27 13:33:21 +0200

    Added Contact form

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -22,7 +22,56 @@
     </div>
   </div>
   <div class="contact-form-container">
+    <h3 class="form-title"><b>Envíame Un Mensaje</b></h3>
+    <b-form class="row g-3 form">
+      <b-form-group
+          id="input-group-name"
+          class="col-md-6">
+        <b-form-input
+            id="input-name"
+            type="text"
+            placeholder="Nombre"
+            required>
+        </b-form-input>
+      </b-form-group>
 
+      <b-form-group
+          id="input-group-email"
+          class="col-md-6">
+        <b-form-input
+            id="input-email"
+            type="email"
+            placeholder="Correo electrónico"
+            required>
+
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group
+          id="input-group-subject"
+          class="col-md-12">
+        <b-form-input
+            id="input-subject"
+            type="text"
+            placeholder="Asunto">
+        </b-form-input>
+      </b-form-group>
+
+      <b-form-group
+          id="input-group-message"
+          class="col-md-12">
+      <b-form-textarea
+          id="textarea"
+          rows="5"
+          placeholder="Mensaje">
+      </b-form-textarea>
+      </b-form-group>
+
+      <b-button type="submit" squared class="send-message-button border-0 ms-2">
+        <b-icon icon="cursor"></b-icon>
+        <p>Enviar Mensaje</p>
+      </b-button>
+    </b-form>
   </div>
 </div>
 </template>
@@ -41,11 +90,13 @@ export default {
 }
 
 .contact .contact-text-container {
-  width: 50%;
+  width: 35%;
+  margin-top: 160px;
+  margin-right: -70px;
 }
 
 .contact .contact-form-container {
-  width: 50%;
+  width: 65%;
 }
 
 .contact .contact-text-container .contact-me-text {
@@ -53,7 +104,6 @@ export default {
   background-color: #d7d7d7;
   padding: 30px 50px 50px 50px;
   border-bottom: 7px solid #68a506;
-  width: 70%;
 }
 
 
@@ -109,7 +159,7 @@ export default {
 }
 
 .contact .contact-text-container .contact-alternative {
-  width: 70%;
+  width: 75%;
   text-align: center;
   margin-top: 20px;
 }
@@ -139,8 +189,8 @@ export default {
 }
 
 .contact .contact-text-container .contact-alternative .linkedin-github .sites .site-container {
-  width: 60px;
-  height: 60px;
+  width: 45px;
+  height: 45px;
   background-color: #888888;
   display: flex;
   align-items: center;
@@ -150,12 +200,52 @@ export default {
 
 .contact .contact-text-container .contact-alternative .linkedin-github .sites .site-container:hover {
   background-color: #68a506;
-
+  transition: all 0.5s ease-out;
 }
 
 .contact .contact-text-container .contact-alternative .linkedin-github .sites .site-container .icon {
   color: white;
-  font-size: 150%;
+  font-size: 130%;
+}
+
+.contact .contact-form-container {
+  background-color: black;
+  padding: 150px 100px 200px 140px;
+}
+
+.contact .contact-form-container h3.form-title {
+  margin-bottom: 30px;
+  color: white;
+}
+
+.contact .contact-form-container .form .form-control {
+  padding: 1rem;
+  border-radius: 0;
+  border: 2px solid #d7d7d7;
+  background-color: black;
+}
+
+.contact .contact-form-container .form .form-control::placeholder {
+  color: #d7d7d7;
+}
+
+.contact .contact-form-container .form .send-message-button {
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  background-color: #68a506;
+  width: fit-content;
+}
+
+.contact .contact-form-container .form .send-message-button p {
+  margin-bottom: 0;
+  margin-left: 5px;
+}
+
+.contact .contact-form-container .form .send-message-button:hover {
+  transition: all 0.5s ease-out;
+  color: black;
+  background-color: white;
 }
 
 
