f6f6e2069a6d3f80b9b52d82875d19f33d27e087
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2024-05-08 21:06:14 +0200

    Changes in skill component

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -3,79 +3,231 @@
     <div class="tag-list">
       <div class="loop-slider" style="--duration:15951ms; --direction:normal;">
         <div class="inner">
-          <div class="tag"><span>#</span> JavaScript</div>
-          <div class="tag"><span>#</span> CSS</div>
-          <div class="tag"><span>#</span> HTML</div>
-          <div class="tag"><span>#</span> Vue.js</div>
-          <div class="tag"><span>#</span> Nuxt</div>
+          <div class="tag" style="--background-color: #F7DF1E; --box-shadow-color: rgba(247, 223, 30, 0.5)">
+            <span>#</span>
+            JavaScript
+            <img class="skill-icon" src="../assets/skills-svg/javascript.svg" alt="JavaScript icon">
+          </div>
+          <div class="tag" style="--background-color: #4FC08D; --box-shadow-color: rgba(79, 192, 141, 0.5)">
+            <span>#</span> Vue.js
+            <img class="skill-icon" src="../assets/skills-svg/vuedotjs.svg" alt="Vue icon">
+          </div>
+          <div class="tag" style="--background-color: #1572B6; --box-shadow-color: rgba(21, 114, 182, 0.5)">
+            <span>#</span> CSS
+            <img class=" skill-icon" src="../assets/skills-svg/css3.svg" alt="CSS icon">
+          </div>
+          <div class="tag" style="--background-color: #E34F26; --box-shadow-color: rgba(227, 79, 38, 0.5)">
+            <span>#</span> HTML
+            <img class="skill-icon" src="../assets/skills-svg/html5.svg" alt="HTML icon">
+          </div>
+          <div class="tag" style="--background-color: #00DC82; --box-shadow-color: rgba(0, 220, 130, 0.5)">
+            <span>#</span> Nuxt
+            <img class="skill-icon" src="../assets/skills-svg/nuxtdotjs.svg" alt="Nuxt icon">
+          </div>
 
-          <div class="tag"><span>#</span> JavaScript</div>
-          <div class="tag"><span>#</span> CSS</div>
-          <div class="tag"><span>#</span> HTML</div>
-          <div class="tag"><span>#</span> Vue.js</div>
-          <div class="tag"><span>#</span> Nuxt</div>
+          <div class="tag" style="--background-color: #F7DF1E; --box-shadow-color: rgba(247, 223, 30, 0.5)">
+            <span>#</span>
+            JavaScript
+            <img class="skill-icon" src="../assets/skills-svg/javascript.svg" alt="JavaScript icon">
+          </div>
+          <div class="tag" style="--background-color: #4FC08D; --box-shadow-color: rgba(79, 192, 141, 0.5)">
+            <span>#</span> Vue.js
+            <img class="skill-icon" src="../assets/skills-svg/vuedotjs.svg" alt="Vue icon">
+          </div>
+          <div class="tag" style="--background-color: #1572B6; --box-shadow-color: rgba(21, 114, 182, 0.5)">
+            <span>#</span> CSS
+            <img class=" skill-icon" src="../assets/skills-svg/css3.svg" alt="CSS icon">
+          </div>
+          <div class="tag" style="--background-color: #E34F26; --box-shadow-color: rgba(227, 79, 38, 0.5)">
+            <span>#</span> HTML
+            <img class="skill-icon" src="../assets/skills-svg/html5.svg" alt="HTML icon">
+          </div>
+          <div class="tag" style="--background-color: #00DC82; --box-shadow-color: rgba(0, 220, 130, 0.5)">
+            <span>#</span> Nuxt
+            <img class="skill-icon" src="../assets/skills-svg/nuxtdotjs.svg" alt="Nuxt icon">
+          </div>
         </div>
       </div>
       <div class="loop-slider" style="--duration:19260ms; --direction:reverse;">
         <div class="inner">
-          <div class="tag"><span>#</span> PHP</div>
-          <div class="tag"><span>#</span> MySQL</div>
-          <div class="tag"><span>#</span> REST API</div>
-          <div class="tag"><span>#</span> Doctrine</div>
-          <div class="tag"><span>#</span> Java</div>
+          <div class="tag" style="--background-color: #777BB4; --box-shadow-color: rgba(119, 123, 180, 0.5)">
+            <span>#</span> PHP
+            <img class="skill-icon" src="../assets/skills-svg/php.svg" alt="PHP icon">
+          </div>
+          <div class="tag" style="--background-color: #4479A1; --box-shadow-color: rgba(68, 121, 161, 0.5)">
+            <span>#</span> MySQL
+            <img class="skill-icon" src="../assets/skills-svg/mysql.svg" alt="MySQL icon">
+          </div>
+          <div class="tag" style="--background-color: #FF4F8B; --box-shadow-color: rgba(255, 79, 139, 0.5)">
+            <span>#</span> REST API
+            <img class="skill-icon" src="../assets/skills-svg/restapi.svg" alt="REST API icon">
+          </div>
+          <div class="tag" style="--background-color: #FF6C37; --box-shadow-color: rgba(255, 108, 55, 0.5)">
+            <span>#</span> Postman
+            <img class="skill-icon" src="../assets/skills-svg/postman.svg" alt="Postman icon">
+          </div>
+          <div class="tag" style="--background-color: #EA2A30; --box-shadow-color: rgba(234, 42, 48, 0.5)">
+            <span>#</span> Java
+            <img class="skill-icon" src="../assets/skills-svg/java.svg" alt="Java icon">
+          </div>
 
-          <div class="tag"><span>#</span> PHP</div>
-          <div class="tag"><span>#</span> MySQL</div>
-          <div class="tag"><span>#</span> REST API</div>
-          <div class="tag"><span>#</span> Doctrine</div>
-          <div class="tag"><span>#</span> Java</div>
+          <div class="tag" style="--background-color: #777BB4; --box-shadow-color: rgba(119, 123, 180, 0.5)">
+            <span>#</span> PHP
+            <img class="skill-icon" src="../assets/skills-svg/php.svg" alt="PHP icon">
+          </div>
+          <div class="tag" style="--background-color: #4479A1; --box-shadow-color: rgba(68, 121, 161, 0.5)">
+            <span>#</span> MySQL
+            <img class="skill-icon" src="../assets/skills-svg/mysql.svg" alt="MySQL icon">
+          </div>
+          <div class="tag" style="--background-color: #FF4F8B; --box-shadow-color: rgba(255, 79, 139, 0.5)">
+            <span>#</span> REST API
+            <img class="skill-icon" src="../assets/skills-svg/restapi.svg" alt="REST API icon">
+          </div>
+          <div class="tag" style="--background-color: #FF6C37; --box-shadow-color: rgba(255, 108, 55, 0.5)">
+            <span>#</span> Postman
+            <img class="skill-icon" src="../assets/skills-svg/postman.svg" alt="Postman icon">
+          </div>
+          <div class="tag" style="--background-color: #EA2A30; --box-shadow-color: rgba(234, 42, 48, 0.5)">
+            <span>#</span> Java
+            <img class="skill-icon" src="../assets/skills-svg/java.svg" alt="Java icon">
+          </div>
         </div>
       </div>
       <div class="loop-slider" style="--duration:10449ms; --direction:normal;">
         <div class="inner">
-          <div class="tag"><span>#</span> jQuery</div>
-          <div class="tag"><span>#</span> Bootstrap</div>
-          <div class="tag"><span>#</span> Axios</div>
-          <div class="tag"><span>#</span> Git</div>
-          <div class="tag"><span>#</span> Strapi</div>
+          <div class="tag" style="--background-color: #0769AD; --box-shadow-color: rgba(7, 105, 173, 0.5)">
+            <span>#</span> jQuery
+            <img class="skill-icon" src="../assets/skills-svg/jquery.svg" alt="jQuery icon">
+          </div>
+          <div class="tag" style="--background-color: #7952B3; --box-shadow-color: rgba(121, 82, 179, 0.5)">
+            <span>#</span> Bootstrap
+            <img class="skill-icon" src="../assets/skills-svg/bootstrap.svg" alt="Bootstrap icon">
+          </div>
+          <div class="tag" style="--background-color: #5A29E4; --box-shadow-color: rgba(90, 41, 228, 0.5)">
+            <span>#</span> Axios
+            <img class="skill-icon" src="../assets/skills-svg/axios.svg" alt="Axios icon">
+          </div>
+          <div class="tag" style="--background-color: #F05032; --box-shadow-color: rgba(240, 80, 50, 0.5)">
+            <span>#</span> Git
+            <img class="skill-icon" src="../assets/skills-svg/git.svg" alt="Git icon">
+          </div>
+          <div class="tag" style="--background-color: #4945FF; --box-shadow-color: rgba(73, 69, 255, 0.5)">
+            <span>#</span> Strapi
+            <img class="skill-icon" src="../assets/skills-svg/strapi.svg" alt="Strapi icon">
+          </div>
 
-          <div class="tag"><span>#</span> jQuery</div>
-          <div class="tag"><span>#</span> Bootstrap</div>
-          <div class="tag"><span>#</span> Axios</div>
-          <div class="tag"><span>#</span> Git</div>
-          <div class="tag"><span>#</span> Strapi</div>
+          <div class="tag" style="--background-color: #0769AD; --box-shadow-color: rgba(7, 105, 173, 0.5)">
+            <span>#</span> jQuery
+            <img class="skill-icon" src="../assets/skills-svg/jquery.svg" alt="jQuery icon">
+          </div>
+          <div class="tag" style="--background-color: #7952B3; --box-shadow-color: rgba(121, 82, 179, 0.5)">
+            <span>#</span> Bootstrap
+            <img class="skill-icon" src="../assets/skills-svg/bootstrap.svg" alt="Bootstrap icon">
+          </div>
+          <div class="tag" style="--background-color: #5A29E4; --box-shadow-color: rgba(90, 41, 228, 0.5)">
+            <span>#</span> Axios
+            <img class="skill-icon" src="../assets/skills-svg/axios.svg" alt="Axios icon">
+          </div>
+          <div class="tag" style="--background-color: #F05032; --box-shadow-color: rgba(240, 80, 50, 0.5)">
+            <span>#</span> Git
+            <img class="skill-icon" src="../assets/skills-svg/git.svg" alt="Git icon">
+          </div>
+          <div class="tag" style="--background-color: #4945FF; --box-shadow-color: rgba(73, 69, 255, 0.5)">
+            <span>#</span> Strapi
+            <img class="skill-icon" src="../assets/skills-svg/strapi.svg" alt="Strapi icon">
+          </div>
         </div>
       </div>
 
       <div class="loop-slider" style="--duration:16638ms; --direction:reverse;">
         <div class="inner">
-          <div class="tag"><span>#</span> Vue.js</div>
-          <div class="tag"><span>#</span> JavaScript</div>
-          <div class="tag"><span>#</span> CSS</div>
-          <div class="tag"><span>#</span> HTML</div>
-          <div class="tag"><span>#</span> jQuery</div>
+          <div class="tag" style="--background-color: #4FC08D; --box-shadow-color: rgba(79, 192, 141, 0.5)">
+            <span>#</span> Vue.js
+            <img class="skill-icon" src="../assets/skills-svg/vuedotjs.svg" alt="Vue icon">
+          </div>
+          <div class="tag" style="--background-color: #F7DF1E; --box-shadow-color: rgba(247, 223, 30, 0.5)">
+            <span>#</span> JavaScript
+            <img class="skill-icon" src="../assets/skills-svg/javascript.svg" alt="JavaScript icon">
+          </div>
+          <div class="tag" style="--background-color: #1572B6; --box-shadow-color: rgba(21, 114, 182, 0.5)">
+            <span>#</span> CSS
+            <img class=" skill-icon" src="../assets/skills-svg/css3.svg" alt="CSS icon">
+          </div>
+          <div class="tag" style="--background-color: #E34F26; --box-shadow-color: rgba(227, 79, 38, 0.5)">
+            <span>#</span> HTML
+            <img class="skill-icon" src="../assets/skills-svg/html5.svg" alt="HTML icon">
+          </div>
+          <div class="tag" style="--background-color: #0769AD; --box-shadow-color: rgba(7, 105, 173, 0.5)">
+            <span>#</span> jQuery
+            <img class="skill-icon" src="../assets/skills-svg/jquery.svg" alt="jQuery icon">
+          </div>
 
-          <div class="tag"><span>#</span> Vue.js</div>
-          <div class="tag"><span>#</span> JavaScript</div>
-          <div class="tag"><span>#</span> CSS</div>
-          <div class="tag"><span>#</span> HTML</div>
-          <div class="tag"><span>#</span> jQuery</div>
+          <div class="tag" style="--background-color: #4FC08D; --box-shadow-color: rgba(79, 192, 141, 0.5)">
+            <span>#</span> Vue.js
+            <img class="skill-icon" src="../assets/skills-svg/vuedotjs.svg" alt="Vue icon">
+          </div>
+          <div class="tag" style="--background-color: #F7DF1E; --box-shadow-color: rgba(247, 223, 30, 0.5)">
+            <span>#</span> JavaScript
+            <img class="skill-icon" src="../assets/skills-svg/javascript.svg" alt="JavaScript icon">
+          </div>
+          <div class="tag" style="--background-color: #1572B6; --box-shadow-color: rgba(21, 114, 182, 0.5)">
+            <span>#</span> CSS
+            <img class=" skill-icon" src="../assets/skills-svg/css3.svg" alt="CSS icon">
+          </div>
+          <div class="tag" style="--background-color: #E34F26; --box-shadow-color: rgba(227, 79, 38, 0.5)">
+            <span>#</span> HTML
+            <img class="skill-icon" src="../assets/skills-svg/html5.svg" alt="HTML icon">
+          </div>
+          <div class="tag" style="--background-color: #0769AD; --box-shadow-color: rgba(7, 105, 173, 0.5)">
+            <span>#</span> jQuery
+            <img class="skill-icon" src="../assets/skills-svg/jquery.svg" alt="jQuery icon">
+          </div>
         </div>
       </div>
 
       <div class="loop-slider" style="--duration:15936ms; --direction:normal;">
         <div class="inner">
-          <div class="tag"><span>#</span> Git</div>
-          <div class="tag"><span>#</span> PHP</div>
-          <div class="tag"><span>#</span> Java</div>
-          <div class="tag"><span>#</span> MySQL</div>
-          <div class="tag"><span>#</span> Doctrine</div>
+          <div class="tag" style="--background-color: #F05032; --box-shadow-color: rgba(240, 80, 50, 0.5)">
+            <span>#</span> Git
+            <img class="skill-icon" src="../assets/skills-svg/git.svg" alt="Git icon">
+          </div>
+          <div class="tag" style="--background-color: #777BB4; --box-shadow-color: rgba(119, 123, 180, 0.5)">
+            <span>#</span> PHP
+            <img class="skill-icon" src="../assets/skills-svg/php.svg" alt="PHP icon">
+          </div>
+          <div class="tag" style="--background-color: #EA2A30; --box-shadow-color: rgba(234, 42, 48, 0.5)">
+            <span>#</span> Java
+            <img class="skill-icon" src="../assets/skills-svg/java.svg" alt="Java icon">
+          </div>
+          <div class="tag" style="--background-color: #4479A1; --box-shadow-color: rgba(68, 121, 161, 0.5)">
+            <span>#</span> MySQL
+            <img class="skill-icon" src="../assets/skills-svg/mysql.svg" alt="MySQL icon">
+          </div>
+          <div class="tag" style="--background-color: #FF6C37; --box-shadow-color: rgba(255, 108, 55, 0.5)">
+            <span>#</span> Postman
+            <img class="skill-icon" src="../assets/skills-svg/postman.svg" alt="Postman icon">
+          </div>
 
-          <div class="tag"><span>#</span> Git</div>
-          <div class="tag"><span>#</span> PHP</div>
-          <div class="tag"><span>#</span> Java</div>
-          <div class="tag"><span>#</span> MySQL</div>
-          <div class="tag"><span>#</span> Doctrine</div>
+          <div class="tag" style="--background-color: #F05032; --box-shadow-color: rgba(240, 80, 50, 0.5)">
+            <span>#</span> Git
+            <img class="skill-icon" src="../assets/skills-svg/git.svg" alt="Git icon">
+          </div>
+          <div class="tag" style="--background-color: #777BB4; --box-shadow-color: rgba(119, 123, 180, 0.5)">
+            <span>#</span> PHP
+            <img class="skill-icon" src="../assets/skills-svg/php.svg" alt="PHP icon">
+          </div>
+          <div class="tag" style="--background-color: #EA2A30; --box-shadow-color: rgba(234, 42, 48, 0.5)">
+            <span>#</span> Java
+            <img class="skill-icon" src="../assets/skills-svg/java.svg" alt="Java icon">
+          </div>
+          <div class="tag" style="--background-color: #4479A1; --box-shadow-color: rgba(68, 121, 161, 0.5)">
+            <span>#</span> MySQL
+            <img class="skill-icon" src="../assets/skills-svg/mysql.svg" alt="MySQL icon">
+          </div>
+          <div class="tag" style="--background-color: #FF6C37; --box-shadow-color: rgba(255, 108, 55, 0.5)">
+            <span>#</span> Postman
+            <img class="skill-icon" src="../assets/skills-svg/postman.svg" alt="Postman icon">
+          </div>
         </div>
       </div>
       <div class="fade-skills"></div>
@@ -105,7 +257,7 @@ body {
 
 
 .skills .tag-list {
-  width: 30rem;
+  width: 38rem;
   max-width: 90vw;
   display: flex;
   flex-shrink: 0;
@@ -129,24 +281,56 @@ body {
 
 .skills .tag {
   display: flex;
+  position: relative;
   align-items: center;
   gap: 0 0.2rem;
   color: #e2e8f0;
-  font-size: 0.9rem;
+  font-size: 1rem;
   background-color: black;
-  padding: 0.3rem 1rem;
+  padding: 0.5rem 1.3rem;
   margin-right: 1rem;
   white-space: nowrap;
   cursor: pointer;
   box-shadow: 0 0.1rem 0.2rem rgb(0 0 0 / 20%),
     0 0.1rem 0.5rem rgb(0 0 0 / 30%),
     0 0.2rem 1.5rem rgb(0 0 0 / 40%);
+  justify-content: center;
+  border: 3px solid black;
+  transition: background 0.3s, color 0.3s, box-shadow 0.5s, border 0.6s;
+  overflow: hidden;
 
+}
+
+.skills .tag .skill-icon {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  opacity: 0;
+  transition: 0.3s ease;
+  transform: translateY(30px);
+}
+
+.skills .tag:hover .skill-icon {
+  display: block;
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.skills .tag:hover {
+  color: black;
+  background-color: black;
+  box-shadow: 0 0 0 5px var(--box-shadow-color);
+  border-color: var(--background-color);
+}
+
+.skills .tag:hover span {
+  color: black;
 }
 
 .skills .tag span {
   font-size: 1.2rem;
   color: #64748b;
+  transition: color 0.1s
 }
 
 .skills .fade-skills {
