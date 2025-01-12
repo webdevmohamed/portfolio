c5a0c09f00f6bf2a34302bf128494a3eb2e428f3
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-01-12 17:53:11 +0100

    Refactor project structure: removed unused components, integrated Tailwind CSS, and added PostCSS configuration. Introduced new Header component and simplified App.vue layout.

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="p-5 bg-white">
+      <h1 class="text-2xl font-semibold text-black">My Portfolio</h1>
+    </header>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style scoped>
+
+  </style>
