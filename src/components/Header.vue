678591fc88bea78bc5a667333d4814a732203b2c
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-01-14 21:53:15 +0100

    Enhance header layout and introduce Navbar component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,19 @@
 <template>
     <header class="p-5 bg-white">
-      <h1 class="text-2xl font-semibold text-black">My Portfolio</h1>
+      <div class="mx-auto container flex items-center justify-between">
+        <div class="flex items-center gap-2">
+          <div class="w-8 h-8 bg-black rounded-full flex items-center justify-center">
+            <p class="text-white text-xl">M</p>
+          </div>
+          <p class="text-md font-bold">Mohamed | Mortahil Elaaouad</p>
+        </div>
+        <Navbar />
+      </div>
     </header>
   </template>
   
   <script setup>
+  import Navbar from './Navbar.vue';
 
   </script>
   
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <div class="mx-auto container">
+      <div class="flex items-center gap-10">
+        <p class="text-md font-bold">Inicio</p>
+        <p class="text-md font-bold">Acerca de mí</p>
+        <p class="text-md font-bold">Servicios</p>
+        <p class="text-md font-bold">Experiencia</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
