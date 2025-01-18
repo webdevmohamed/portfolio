8cde32b949e5d389eeb9388c5da925b088142284
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-01-18 13:30:19 +0100

    Update Header logo color from white to foreground for improved visibility and consistency with design standards.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto container flex items-center justify-between">
       <div class="flex items-center gap-3 group cursor-pointer">
         <div class="relative w-8 h-8 bg-primary rounded-xl flex items-center justify-center">
-          <p class="text-white text-xl font-semibold z-[2]">M</p>
+          <p class="text-foreground text-xl font-semibold z-[2]">M</p>
           <div
             class="absolute z-[1] inset-0 bg-primary/60 rounded-xl blur-md transform group-hover:scale-125 transition-transform">
           </div>
