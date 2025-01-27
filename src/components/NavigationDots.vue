79b4b85a41cd7dcf9619ae79e1c8277efff32935
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-01-27 22:21:13 +0100

    Added Sticky scroll to defined sections, hide scrollbar and reformat code in base.css

--- src/components/NavigationDots.vue
+++ src/components/NavigationDots.vue
@@ -2,36 +2,50 @@
     <div class="fixed right-10 top-1/2 transform -translate-y-1/2 flex flex-col items-center">
         <div v-for="(section, index) in sections" :key="index" :class="[
             'w-3 h-3 rounded-sm mb-5 transition-all duration-300 border-2 cursor-pointer',
-            activeSection === section ? 'bg-transparent border-foreground scale-150' : 'bg-primary border-primary rotate-45'
-        ]" @click="scrollToSection(section)"></div>
+            currentSectionIndex === index ? 'bg-transparent border-foreground scale-150' : 'bg-primary border-primary rotate-45'
+        ]" @click="scrollToSection(index)"></div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
-const sections = ['home', 'about', 'services', 'experience', 'contact'];
-const activeSection = ref('home'); // Cambia esto según la sección activa
+const sections = ['home', 'about'];
+const currentSectionIndex = ref(0);
 
-const scrollToSection = (section) => {
+const handleScroll = (event) => {
+    const delta = Math.sign(event.deltaY); // Determina la dirección del scroll
+    if (delta > 0 && currentSectionIndex.value < sections.length - 1) {
+        // Scroll hacia abajo
+        currentSectionIndex.value++;
+    } else if (delta < 0 && currentSectionIndex.value > 0) {
+        // Scroll hacia arriba
+        currentSectionIndex.value--;
+    }
+
+    const targetSection = document.getElementById(sections[currentSectionIndex.value]);
+    if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+};
+
+
+const scrollToSection = (sectionIndex) => {
+    currentSectionIndex.value = sectionIndex;
+    const section = sections[currentSectionIndex.value];
     const element = document.getElementById(section);
     if (element) {
         element.scrollIntoView({ behavior: 'smooth' });
     }
 };
 
-// Escucha el evento de scroll para actualizar la sección activa
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-    sections.forEach((section) => {
-        const element = document.getElementById(section);
-        if (element) {
-            const { top, bottom } = element.getBoundingClientRect();
-            if (top <= window.innerHeight / 2 && bottom >= window.innerHeight / 2) {
-                activeSection.value = section;
-            }
-        }
-    });
+
+onMounted(() => {
+    window.addEventListener('wheel', handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('wheel', handleScroll);
 });
 </script>
 
