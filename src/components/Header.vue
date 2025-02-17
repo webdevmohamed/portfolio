a37b48a6e93ae83931d2662810f9abe01238a424
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-02-17 20:33:04 +0100

    Added transition to header logo when is scrolled

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,10 @@
   ]">
     <div class="mx-auto container flex items-center justify-between">
       <div class="flex items-center gap-3 group cursor-pointer">
-        <div class="relative w-8 h-8 bg-primary rounded-xl flex items-center justify-center">
-          <p class="text-foreground text-xl font-semibold z-[2]">M</p>
+        <div class="relative w-8 h-8 bg-primary rounded-xl flex items-center justify-center transition-all duration-300"
+        :class="[isScrolled ? 'rotate-45 scale-105' : '' ]">
+          <p class="text-foreground text-xl font-semibold z-[2] transition-all duration-300"
+             :class="[isScrolled ? '-rotate-45' : '' ]">M</p>
           <div
             class="absolute z-[1] inset-0 bg-primary/60 rounded-xl blur-md transform group-hover:scale-125 transition-transform">
           </div>
