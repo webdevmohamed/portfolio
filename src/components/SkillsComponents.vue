1f7049a8c392d6fdfb88f91ddd2c4926ce2a61ce
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-19 13:01:46 +0200

    Refactor skills section to improve responsiveness and added category icons for small devices

--- src/components/SkillsComponents.vue
+++ src/components/SkillsComponents.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="skills" class="relative min-h-screen p-5">
-    <div class="container mx-auto max-w-screen-lg">
-      <h1 class="text-5xl text-center font-bold text-foreground mb-4">
+  <div id="skills" class="relative  md:min-h-screen py-5 px-5 max-md:py-16 max-md:px-20 max-sm:px-16 max-xs:px-12 max-xxs:px-5">
+    <div class="container mx-auto xl:max-w-screen-lg max-w-screen-md relative flex flex-col items-center justify-center max-md:items-start lg:px-0 md:px-[30px]">
+      <h1 class="text-5xl max-md:text-4xl font-bold text-foreground mb-4">
         {{ t('skills.title') }}
       </h1>
 
-      <p class="text-md text-center text-foreground/80 mb-12 max-w-2xl mx-auto">
+      <p class="text-md text-center max-md:text-left text-foreground/80 mb-12 max-w-2xl mx-auto">
         {{ t('skills.description') }}
       </p>
 
@@ -13,15 +13,16 @@
         <button
           v-for="category in categories"
           :key="category"
-          @click="currentCategory = category"
+          @click="currentCategory = category.name"
           class="px-4 py-2 rounded-full text-sm transition-all"
-          :class="{ 'bg-primary/20 border-primary text-foreground': currentCategory === category }"
+          :class="{ 'bg-primary/15 border-primary text-primary-500': currentCategory === category.name }"
         >
-          {{ t(`skills.categories.${category}`) }}
+          <span class="max-sm:hidden">{{ t(`skills.categories.${category.name}`) }}</span>
+          <Icon :icon="category.icon" class="w-5 h-5 sm:hidden " />
         </button>
       </div>
 
-      <div class="flex flex-wrap justify-center gap-5">
+      <div class="flex flex-wrap justify-center max-md:justify-start gap-5">
         <TransitionGroup name="skill-fade">
           <a
             v-for="skill in filteredSkills"
@@ -29,14 +30,14 @@
             :href="skill.url"
             target="_blank"
             rel="noopener noreferrer"
-            class="relative overflow-hidden group flex items-center justify-center p-3 rounded-full ring-2 ring-transparent bg-gradient-to-b from-accent-blue/20 to-primary/20 border-transparent hover:ring-foreground hover:bg-none transition-all duration-300 hover:scale-105 hover:shadow-lg w-20 md:w-24 aspect-square"
+            class="relative overflow-hidden group flex items-center justify-center p-3 rounded-full ring-2 ring-transparent bg-gradient-to-b from-accent-blue/20 to-primary/20 border-transparent hover:ring-foreground hover:bg-none transition-all duration-300 hover:scale-105 hover:shadow-lg w-24 max-xl:w-20 max-xs:w-16 aspect-square"
           >
             <div class="flex flex-col items-center gap-2">
               <Icon
                 :icon="skill.icon"
-                class="w-10 h-10 transition-all duration-300 group-hover:-translate-y-10 group-hover:opacity-0"
+                class="w-10 h-auto max-xl:w-9 max-xs:w-7 transition-all duration-300 group-hover:-translate-y-10 group-hover:opacity-0"
               />
-              <span class="text-xs font-semibold absolute opacity-0 top-1/2 transform group-hover:opacity-100 group-hover:-translate-y-1/2 transition-all duration-300">{{ skill.name }}</span>
+              <span class="text-xs max-xs:text-[10px] font-semibold absolute opacity-0 top-1/2 transform group-hover:opacity-100 group-hover:-translate-y-1/2 transition-all duration-300">{{ skill.name }}</span>
             </div>
           </a>
         </TransitionGroup>
@@ -52,7 +53,12 @@ import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 
-const categories = ['all', 'frontend', 'backend', 'tools']
+const categories = [
+  {name: 'all', icon: 'mdi:view-grid-outline'},
+  {name: 'frontend', icon: 'mdi:monitor-dashboard'},
+  {name: 'backend', icon: 'mdi:server'},
+  {name: 'tools', icon: 'mdi:tools'}
+]
 const currentCategory = ref('all')
 
 const skills = [
