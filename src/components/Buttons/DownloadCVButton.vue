d718542e7eedea02bb7017b795af189f1cecced5
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-19 18:55:22 +0200

    Improve about section buttons styles

--- src/components/Buttons/DownloadCVButton.vue
+++ src/components/Buttons/DownloadCVButton.vue
@@ -2,10 +2,9 @@
   <a href="@/assets/Mohamed_Mortahil_Elaaouad_Junio_2024.pdf" download
      class="group relative flex items-center gap-2 px-6 py-3 max-xl:px-4 max-xl:py-2 bg-background rounded-xl
                shadow-lg hover:shadow-primary/50 dark:hover:shadow-primary/20 transition-all duration-300">
-    <Icon icon="mdi:tray-download" class="flex-shrink-0 w-5 h-auto text-foreground group-hover:text-primary-400 transition-all duration-300" />
+    <Icon icon="mdi:tray-download" class="flex-shrink-0 w-5 h-auto text-foreground text-primary-400" />
     <span class="font-semibold text-foreground max-xl:text-sm">{{ t('about.downloadCV') }}</span>
-    <div class="absolute inset-0 rounded-xl border-2 border-foreground dark:border-white/10
-                    group-hover:border-primary/30 transition-colors duration-300">
+    <div class="absolute inset-0 rounded-xl border-2 border-primary/30 transition-colors duration-300">
     </div>
   </a>
 </template>
--- src/components/Buttons/GithubButton.vue
+++ src/components/Buttons/GithubButton.vue
@@ -2,10 +2,9 @@
   <a href="https://github.com/your-github" target="_blank"
      class="group relative flex items-center gap-2 p-3 max-xl:p-2 max-lg:px-4 max-lg:py-2 bg-background rounded-xl
                shadow-lg hover:shadow-primary/50 dark:hover:shadow-primary/20 transition-all duration-300">
-    <Icon icon="mdi:github" class="flex-shrink-0 w-6 max-xl:w-5 h-auto text-foreground group-hover:text-primary-400 transition-all duration-300" />
+    <Icon icon="mdi:github" class="flex-shrink-0 w-6 max-xl:w-5 h-auto text-primary-400" />
     <span class="lg:hidden font-semibold relative z-10 max-xl:text-sm">GitHub</span>
-    <div class="absolute inset-0 rounded-xl border-2 border-foreground dark:border-white/10
-                    group-hover:border-primary/30 transition-colors duration-300">
+    <div class="absolute inset-0 rounded-xl border-2 border-primary/30 transition-colors duration-300">
     </div>
   </a>
 </template>
