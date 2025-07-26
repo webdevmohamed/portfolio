ac840b484fba1b2f2d17c7fd0777f0eedbe39f95
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-26 13:30:16 +0200

    Refactor button styles for contact, download, and GitHub links

--- src/components/Buttons/ContactMeButton.vue
+++ src/components/Buttons/ContactMeButton.vue
@@ -2,7 +2,7 @@
   <a href="mailto:your-email@example.com"
      class="group relative flex items-center gap-2 px-6 py-3 max-xl:px-4 max-xl:py-2
                bg-primary/80 text-white rounded-xl overflow-hidden
-               shadow-lg hover:shadow-primary/50 dark:hover:shadow-primary/20 hover:bg-primary transition-all duration-300">
+               shadow-lg shadow-primary/50 dark:shadow-primary/20 hover:bg-primary hover:shadow-none transition-all duration-300">
     <Icon icon="mdi:envelope-outline" class="flex-shrink-0 w-5 h-auto text-white/90 relative z-10" />
     <span class="font-semibold relative z-10 max-xl:text-sm">{{ t('about.contactMe') }}</span>
     <div class="absolute inset-0 border-2 border-white/20 group-hover:border-primary rounded-xl transition-all duration-300"></div>
--- src/components/Buttons/DownloadCVButton.vue
+++ src/components/Buttons/DownloadCVButton.vue
@@ -1,11 +1,10 @@
 <template>
   <a href="@/assets/Mohamed_Mortahil_Elaaouad_Junio_2024.pdf" download
      class="group relative flex items-center gap-2 px-6 py-3 max-xl:px-4 max-xl:py-2 bg-background rounded-xl
-               shadow-lg hover:shadow-primary/50 dark:hover:shadow-primary/20 transition-all duration-300">
-    <Icon icon="mdi:tray-download" class="flex-shrink-0 w-5 h-auto text-foreground group-hover:text-primary-400 transition-all duration-300" />
+               shadow-lg shadow-primary/50 dark:shadow-primary/20 hover:shadow-none transition-all duration-300">
+    <Icon icon="mdi:tray-download" class="flex-shrink-0 w-5 h-auto text-primary-400 group-hover:text-foreground transition-all duration-300" />
     <span class="font-semibold text-foreground max-xl:text-sm">{{ t('about.downloadCV') }}</span>
-    <div class="absolute inset-0 rounded-xl border-2 border-black/10 dark:border-white/10
-                    group-hover:border-primary/30 transition-colors duration-300">
+    <div class="absolute inset-0 rounded-xl border-2 border-primary/30 hover:border-foreground transition-colors duration-300">
     </div>
   </a>
 </template>
