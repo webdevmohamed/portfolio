b55c93e3ee1c4d657b758cc77da811c78258f024
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-27 13:52:13 +0200

    Refactor ContactMeButton styles for improved hover text color consistency

--- src/components/Buttons/ContactMeButton.vue
+++ src/components/Buttons/ContactMeButton.vue
@@ -2,8 +2,8 @@
   <a href="mailto:your-email@example.com"
      class="group relative flex items-center max-xs:justify-between max-xs:flex-row-reverse gap-2 px-5 py-2 max-xl:px-4
                bg-primary/10 text-primary rounded-xl border-2 border-primary hover:bg-primary transition-all duration-300">
-    <Icon icon="mdi:envelope-outline" class="flex-shrink-0 w-5 h-auto text-primary group-hover:text-foreground transition-all duration-300" />
-    <span class="font-semibold max-xl:text-sm group-hover:text-foreground transition-all duration-300">{{ t('about.contactMe') }}</span></a>
+    <Icon icon="mdi:envelope-outline" class="flex-shrink-0 w-5 h-auto text-primary group-hover:text-white transition-all duration-300" />
+    <span class="font-semibold max-xl:text-sm group-hover:text-white transition-all duration-300">{{ t('about.contactMe') }}</span></a>
 </template>
 
 <script setup>
