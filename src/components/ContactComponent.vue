b1f2fb689686634a7dce5fab296063403089958a
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-28 21:35:31 +0200

    Refactor ContactComponent styles for improved layout, responsiveness, and text size adjustments

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,30 +1,30 @@
 <template>
-  <div id="contact" class="relative min-h-screen">
-    <div class="container mx-auto max-w-screen-lg relative flex flex-col items-center justify-center min-h-screen">
-      <h1 class="text-5xl text-center font-bold bg-clip-text text-foreground">
+  <div id="contact" class="relative md:min-h-screen py-5 px-5 max-md:py-16 max-md:px-20 max-sm:px-16 max-xs:px-12 max-xxs:px-5">
+    <div class="container mx-auto xl:max-w-screen-lg max-w-screen-md relative flex flex-col items-center justify-center max-md:items-start lg:px-0 md:px-[30px]">
+      <h1 class="text-5xl max-md:text-4xl font-bold text-foreground mb-4">
         {{ t('contact.subtitle') }}
       </h1>
-      <p class="text-md text-center text-foreground mt-4 max-w-2xl mx-auto">{{ t('contact.description') }}</p>
-      <form class="mt-14 flex flex-col gap-10 w-full max-w-4xl">
-        <div class="grid grid-cols-2 gap-10 w-full">
+      <p class="text-md text-center max-md:text-left text-foreground/80 mb-12 max-w-2xl md:mx-auto">{{ t('contact.description') }}</p>
+      <form class="md:mt-10 flex flex-col gap-10 w-full max-w-4xl">
+        <div class="grid grid-cols-2 max-md:grid-cols-1 gap-10 w-full">
           <div class="flex flex-col gap-2 group">
-            <label for="name" class="text-primary/70 text-sm font-light group-focus-within:text-accent-blue">{{ t('contact.form.label.name') }}</label>
-            <input id="name" type="text" :placeholder="t('contact.form.placeholder.name')" class="py-3 font-light text-primary placeholder:text-primary bg-transparent border-b-2 border-primary focus:text-accent-blue focus:border-accent-blue focus:outline-none focus:placeholder-transparent caret-accent-blue">
+            <label for="name" class="text-primary/70 text-sm max-lg:text-xs font-light group-focus-within:text-accent-blue">{{ t('contact.form.label.name') }}</label>
+            <input id="name" type="text" :placeholder="t('contact.form.placeholder.name')" class="max-lg:text-sm py-3 font-light text-primary placeholder:text-primary bg-transparent border-b-2 border-primary focus:text-accent-blue focus:border-accent-blue focus:outline-none focus:placeholder-transparent caret-accent-blue">
           </div>
           <div class="flex flex-col gap-2 group">
-            <label for="email" class="text-primary/70 text-sm font-light group-focus-within:text-accent-blue">{{ t('contact.form.label.email') }}</label>
-            <input id="email" type="email" :placeholder="t('contact.form.placeholder.email')" class="py-3 font-light text-primary placeholder:text-primary bg-transparent border-b-2 border-primary focus:text-accent-blue focus:border-accent-blue focus:outline-none focus:placeholder-transparent caret-accent-blue">
+            <label for="email" class="text-primary/70 text-sm max-lg:text-xs font-light group-focus-within:text-accent-blue">{{ t('contact.form.label.email') }}</label>
+            <input id="email" type="email" :placeholder="t('contact.form.placeholder.email')" class="max-lg:text-sm py-3 font-light text-primary placeholder:text-primary bg-transparent border-b-2 border-primary focus:text-accent-blue focus:border-accent-blue focus:outline-none focus:placeholder-transparent caret-accent-blue">
           </div>
         </div>
         <div class="flex flex-col gap-2 group">
-          <label for="message" class="text-primary/70 text-sm font-light group-focus-within:text-accent-blue transition-all duration-300">{{ t('contact.form.label.message') }}</label>
-          <textarea id="message" :placeholder="t('contact.form.placeholder.message')" class="py-3 font-light text-primary resize-none placeholder:text-primary bg-transparent border-b-2 border-primary focus:text-accent-blue focus:border-accent-blue focus:outline-none focus:placeholder-transparent caret-accent-blue"></textarea>
+          <label for="message" class="text-primary/70 text-sm max-lg:text-xs font-light group-focus-within:text-accent-blue transition-all duration-300">{{ t('contact.form.label.message') }}</label>
+          <textarea id="message" :placeholder="t('contact.form.placeholder.message')" class="max-lg:text-sm py-3 font-light text-primary resize-none placeholder:text-primary bg-transparent border-b-2 border-primary focus:text-accent-blue focus:border-accent-blue focus:outline-none focus:placeholder-transparent caret-accent-blue"></textarea>
         </div>
 
-        <div class="flex items-center justify-center w-full">
+        <div class="flex items-center md:justify-center w-full">
           <button
             type="submit"
-            class="group flex items-center justify-center gap-3 w-fit px-8 py-4 rounded-full ring-2 ring-transparent bg-gradient-to-r from-primary to-accent-blue hover:bg-none hover:ring-primary transition-all duration-300"
+            class="group flex items-center justify-center gap-3 w-fit px-8 py-4 max-lg:px-6 max-lg:py-3 rounded-full ring-2 ring-transparent bg-gradient-to-r from-primary to-accent-blue hover:bg-none hover:ring-primary transition-all duration-300"
           >
             <span class="text-background font-medium group-hover:text-primary transition-all duration-300">{{ t('contact.form.send') }}</span>
             <Icon
