05a091e9fab360202e20d1cd090a87b6067904c7
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-28 20:59:45 +0200

    Refactor ContactMeButton styles for improved hover effects and color contrast

--- src/components/Buttons/ContactMeButton.vue
+++ src/components/Buttons/ContactMeButton.vue
@@ -1,9 +1,9 @@
 <template>
   <a href="mailto:your-email@example.com"
      class="group relative flex items-center max-xs:justify-between max-xs:flex-row-reverse gap-2 px-5 py-2 max-xl:px-4
-               bg-primary/10 text-primary rounded-xl border-2 border-primary hover:bg-primary transition-all duration-300">
-    <Icon icon="mdi:envelope-outline" class="flex-shrink-0 w-5 h-auto text-primary group-hover:text-white transition-all duration-300" />
-    <span class="font-semibold max-xl:text-sm group-hover:text-white transition-all duration-300">{{ t('about.contactMe') }}</span></a>
+               hover:bg-primary/10 hover:text-primary rounded-xl border-2 border-primary bg-primary transition-all duration-300">
+    <Icon icon="mdi:envelope-outline" class="flex-shrink-0 w-5 h-auto group-hover:text-primary text-white transition-all duration-300" />
+    <span class="font-semibold max-xl:text-sm text-white group-hover:text-primary transition-all duration-300">{{ t('about.contactMe') }}</span></a>
 </template>
 
 <script setup>
