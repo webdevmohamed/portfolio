bdae32e63b9551abcb935a3e2dadda841355b747
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-29 18:58:51 +0200

    Change urls in about section buttons

--- src/components/Buttons/ContactMeButton.vue
+++ src/components/Buttons/ContactMeButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="mailto:your-email@example.com"
+  <a href="mailto:mohamed.mortahil@gmail.com"
      class="group relative flex items-center max-xs:justify-between max-xs:flex-row-reverse gap-2 px-5 py-2 max-xl:px-4
                hover:bg-primary/10 hover:text-primary rounded-xl border-2 border-primary bg-primary transition-all duration-300">
     <Icon icon="mdi:envelope-outline" class="flex-shrink-0 w-5 h-auto group-hover:text-primary text-white transition-all duration-300" />
--- src/components/Buttons/GithubButton.vue
+++ src/components/Buttons/GithubButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/your-github" target="_blank"
+  <a href="https://github.com/webdevmohamed" target="_blank"
      class="group relative flex items-center max-xs:justify-between max-xs:flex-row-reverse gap-2 p-2 max-lg:px-4 bg-foreground border-2 border-foreground rounded-xl transition-all duration-300">
     <Icon icon="mdi:github" class="flex-shrink-0 w-6 max-xl:w-5 h-auto text-background group-hover:text-primary transition-all duration-300" />
     <span class="lg:hidden font-semibold text-background max-xl:text-sm">GitHub</span>
