69eb1d860349349c990bf4ad7faa90dbea8b9adb
Mohamed Mortahil Elaaouad <mohamed.mortahil@gmail.com>
2025-07-29 20:41:46 +0200

    Change cv

--- src/components/Buttons/DownloadCVButton.vue
+++ src/components/Buttons/DownloadCVButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="@/assets/Mohamed_Mortahil_Elaaouad_Junio_2024.pdf" download
+  <a href="@/assets/Mohamed_Mortahil_Elaaouad_Julio_2025.pdf" download
      class="group relative flex items-center max-xs:justify-between max-xs:flex-row-reverse gap-2 px-5 py-2 max-xl:px-4 bg-foreground border-2 border-foreground rounded-xl transition-all duration-300">
     <Icon icon="mdi:tray-download" class="flex-shrink-0 w-5 h-auto text-background group-hover:text-primary transition-all duration-300" />
     <span class="font-semibold text-background max-xl:text-sm">{{ t('about.downloadCV') }}</span>
